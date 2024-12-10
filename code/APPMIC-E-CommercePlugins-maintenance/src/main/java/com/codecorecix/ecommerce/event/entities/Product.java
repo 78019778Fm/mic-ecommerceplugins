37020d1c9f88277ff65b2afcf73f07aadb0d93e8
Luigui Alexander Fuentes Medina <luiguialexander.fuentesmedina@emeal.nttdata.com>
2024-12-10 01:48:30 -0500

    [master]: Se ha actualizado el postman collections

--- code/APPMIC-E-CommercePlugins-maintenance/src/main/java/com/codecorecix/ecommerce/event/entities/Product.java
+++ code/APPMIC-E-CommercePlugins-maintenance/src/main/java/com/codecorecix/ecommerce/event/entities/Product.java
@@ -1,19 +1,15 @@
 package com.codecorecix.ecommerce.event.entities;
 
 import java.io.Serializable;
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -60,20 +56,4 @@ public class Product implements Serializable {
 
   @Column
   private Boolean isRecommended;
-
-  /**
-   * <p>En JPA (Java Persistence API), el tipo de carga por defecto para las relaciones @OneToMany y @ManyToMany es LAZY (perezosa).</p>
-   * <p>Recuerda, no es obligatorio especificar un tipo de carga, pero es buena práctica ser explícito para que el código sea más legible y
-   * fácil de mantener.</p>
-   */
-  @OneToMany(mappedBy = "product", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY, orphanRemoval = true)
-  private List<ProductImage> images;
-
-  /**
-   * <p>En JPA (Java Persistence API), el tipo de carga por defecto para las relaciones @OneToMany y @ManyToMany es LAZY (perezosa).</p>
-   * <p>Recuerda, no es obligatorio especificar un tipo de carga, pero es buena práctica ser explícito para que el código sea más legible y
-   * fácil de mantener.</p>
-   */
-  //  @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
-  //  private List<ProductDetail> details;
 }
