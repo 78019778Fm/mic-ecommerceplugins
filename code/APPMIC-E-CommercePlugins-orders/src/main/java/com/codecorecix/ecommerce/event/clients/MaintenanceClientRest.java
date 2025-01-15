a7bbf752f316093fb4fe42f3c110934ddb3356b5
Luigui Alexander Fuentes Medina <luiguialexander.fuentesmedina@emeal.nttdata.com>
2025-01-15 17:30:41 -0500

    [develop]: Se ha renombrado el name de en los application.properties de los microservicios de ordenes y mantenimiento

--- code/APPMIC-E-CommercePlugins-orders/src/main/java/com/codecorecix/ecommerce/event/clients/MaintenanceClientRest.java
+++ code/APPMIC-E-CommercePlugins-orders/src/main/java/com/codecorecix/ecommerce/event/clients/MaintenanceClientRest.java
@@ -12,7 +12,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "APPMIC-E-CommercePlugins-maintenance")
+@FeignClient(name = "appmic-e-commerceplugins-maintenance")
 public interface MaintenanceClientRest {
 
   @GetMapping("/api/products/checkProducts")
